dac75986dcb216dc6209b757fdd9846dc49f068c
Ziya Göktürk Vatansever <78758157+SynRaito@users.noreply.github.com>
2025-08-14 16:25:26 +0300

    Add files via upload

---
+++ PostProcessController/PVDPostProcessController.cpp
@@ -0,0 +1,161 @@
+﻿#include "PVDPostProcessController.h"
+#include "GESDataTypes.h"
+#include "GESHandler.h"
+#include "Camera/CameraComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "PVD/PVD.h"
+#include "PVD/Characters/PVDCharacter.h"
+
+
+UPVDPostProcessController::UPVDPostProcessController()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+void UPVDPostProcessController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if(auto Character = Cast<APVDCharacter>(GetWorld()->GetFirstPlayerController()->GetPawn()))
+	{
+		if(auto CameraComponent = Character->GetCameraComponent())
+		{
+			PostProcessSettings = &CameraComponent->PostProcessSettings;
+		}
+	}
+	
+	CategorizeConfigsWithEvents();
+	
+	/** Handle any Begin Play triggers, Map will check if any trigger is set as BeginPlay*/
+	GES_POSTPROCESS_EFFECT_EMIT(EPPFXGlobalEvent::PPFX_BeginPlay, GetOwner());
+}
+
+void UPVDPostProcessController::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+	Super::EndPlay(EndPlayReason);
+
+	/** Unbind from GES events */ 
+	FGESHandler::DefaultHandler()->RemoveAllListenersForReceiver(this);
+}
+
+void UPVDPostProcessController::TickComponent(float DeltaTime, ELevelTick TickType,
+                                              FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	
+	ProcessConfigs(DeltaTime);
+}
+
+/** Trigger setup and run */
+void UPVDPostProcessController::CategorizeConfigsWithEvents()
+{
+	for (auto ConfigContainer : ConfigContainers)
+	{
+		if(!EventConfigContainerMap.Contains(ConfigContainer.GlobalEventType))
+		{
+			EventConfigContainerMap.Add(ConfigContainer.GlobalEventType);
+		}
+	
+		EventConfigContainerMap[ConfigContainer.GlobalEventType].ConfigContainers.Add(ConfigContainer);
+		    
+		GES_POSTPROCESS_EFFECT_EVENT_CONTEXT(ConfigContainer.GlobalEventType);
+		FGESHandler::DefaultHandler()->AddLambdaListener(GESEventContext, [this,ConfigContainer] (UObject* InTarget)
+		{
+			if(InTarget == GetOwner())
+			{
+				Run(EventConfigContainerMap[ConfigContainer.GlobalEventType]);
+			}
+		});
+	}
+}
+
+void UPVDPostProcessController::Run(FPostProcessControllerConfigContainers Containers)
+{
+	for (FPostProcessControllerConfigContainer ConfigContainer : Containers.ConfigContainers)
+	{
+		if(ConfigContainer.bTerminateOtherRunningConfigsOnActivate)
+		{
+			for (auto RunningPostProcessControllerConfig : RunningPostProcessControllerConfigs)
+			{
+				RunningPostProcessControllerConfig.End(*PostProcessSettings,RunningPostProcessControllerConfig);
+			}
+			RunningPostProcessControllerConfigs.Empty();
+		}
+		for (auto Config : ConfigContainer.Configs)
+		{
+			Config.PickProcessFunction();
+			Config.Priority = ConfigContainer.Priority;
+			Run(Config);
+		}
+	}
+}
+
+void UPVDPostProcessController::Run(FPostProcessControllerConfig& Config)
+{
+	RunningPostProcessControllerConfigs.Add(Config);
+}
+
+void UPVDPostProcessController::ProcessConfigs(float DeltaTime)
+{
+	for (int iterator = RunningPostProcessControllerConfigs.Num() - 1; iterator >= 0; --iterator)
+	{
+		FPostProcessControllerConfig &RunningPostProcessControllerConfig = RunningPostProcessControllerConfigs[iterator];
+
+		//Process Delay Timer
+		if(RunningPostProcessControllerConfig.DelayTimer < RunningPostProcessControllerConfig.EffectDelay)
+		{
+			RunningPostProcessControllerConfig.EffectDelay += DeltaTime;
+			continue;
+		}
+		
+		RunningPostProcessControllerConfig.EffectTimer += DeltaTime;
+		
+		if(RunningPostProcessControllerConfig.EffectLength >= RunningPostProcessControllerConfig.EffectTimer)
+		{
+			if(CurrentPriorityLevel <= RunningPostProcessControllerConfig.Priority)
+			{
+				CurrentPriorityLevel = RunningPostProcessControllerConfig.Priority;
+				if(ActiveRunningPostProcessControllerConfig != nullptr)
+				{
+					switch (ActiveRunningPostProcessControllerConfig->AttributeType)
+					{
+					case EPostProcessAttributeType::FVector4:
+						if(ActiveRunningPostProcessControllerConfig->bUseBaseValueAsReturnValue)
+							RunningPostProcessControllerConfig.FVector4BaseValue = ActiveRunningPostProcessControllerConfig->FVector4BaseValue;
+						else
+							RunningPostProcessControllerConfig.FVector4BaseValue = ActiveRunningPostProcessControllerConfig->FVector4ReturnValue;
+						break;
+					case EPostProcessAttributeType::Float:
+						if(ActiveRunningPostProcessControllerConfig->bUseBaseValueAsReturnValue)
+							RunningPostProcessControllerConfig.floatBaseValue = ActiveRunningPostProcessControllerConfig->floatBaseValue;
+						else
+							RunningPostProcessControllerConfig.floatBaseValue = ActiveRunningPostProcessControllerConfig->floatReturnValue;
+						break;
+					case EPostProcessAttributeType::FLinearColor:
+						if(ActiveRunningPostProcessControllerConfig->bUseBaseValueAsReturnValue)
+							RunningPostProcessControllerConfig.FLinearColorBaseValue = ActiveRunningPostProcessControllerConfig->FLinearColorBaseValue;
+						else
+							RunningPostProcessControllerConfig.FLinearColorBaseValue = ActiveRunningPostProcessControllerConfig->FLinearColorReturnValue;
+						break;
+					}
+					RunningPostProcessControllerConfig.bIsBaseValueInitialized = true;
+				} 
+				ActiveRunningPostProcessControllerConfig = &RunningPostProcessControllerConfig;
+			}
+		}
+		else
+		{
+			RunningPostProcessControllerConfig.End(*PostProcessSettings, *ActiveRunningPostProcessControllerConfig);
+			if(&RunningPostProcessControllerConfig == ActiveRunningPostProcessControllerConfig)
+			{
+				ActiveRunningPostProcessControllerConfig = nullptr;
+			}
+			RunningPostProcessControllerConfigs.RemoveAt(iterator);
+		}
+	}
+
+	if(ActiveRunningPostProcessControllerConfig != nullptr && !ActiveRunningPostProcessControllerConfig->bIsCompleted)
+	{
+		ActiveRunningPostProcessControllerConfig->Process(*PostProcessSettings, *ActiveRunningPostProcessControllerConfig, DeltaTime);
+	}
+}
---
+++ PostProcessController/PVDPostProcessController.h
@@ -0,0 +1,264 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include <functional>
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "PVDPostProcessController.generated.h"
+
+#define POST_PROCESS_float_BASEVALUESET(ATTRIBUTENAME) Config.floatBaseValue = PostProcessSettings.ATTRIBUTENAME;\
+
+#define POST_PROCESS_FVector4_BASEVALUESET(ATTRIBUTENAME) Config.FVector4BaseValue = PostProcessSettings.ATTRIBUTENAME;\
+
+#define POST_PROCESS_FLinearColor_BASEVALUESET(ATTRIBUTENAME) Config.FLinearColorBaseValue = PostProcessSettings.ATTRIBUTENAME;\
+
+#define POST_PROCESS_ATTRIBUTE_CASE(TYPE, ATTRIBUTENAME) \
+	case EPostProcessAttributes::##ATTRIBUTENAME:\
+	Process = [&](FPostProcessSettings& PostProcessSettings, FPostProcessControllerConfig& Config, float DeltaTime){\
+		TYPE Value = TYPE(); \
+		if(!Config.bIsBaseValueInitialized){\
+			POST_PROCESS_##TYPE##_BASEVALUESET(ATTRIBUTENAME)\
+			Config.bIsBaseValueInitialized = true;\
+		}\
+		if(!Config.bIsTargetValueInitialized){\
+			switch (Config.OperationType) {\
+				case EOperationType::Additive :\
+					Config.TYPE##TargetValue = Config.TYPE##BaseValue + Value; \
+					break;\
+				case EOperationType::Multiply :\
+					Config.TYPE##TargetValue = Config.TYPE##BaseValue * Value; \
+					break;\
+				case EOperationType::Override :\
+					Config.TYPE##TargetValue = Value; \
+					break;\
+			}\
+			Config.bIsTargetValueInitialized = true;\
+		}\
+		switch (Config.CalculationType){\
+			case ECalculationType::Instant : \
+				Value = Config.TYPE##TargetValue;\
+				Config.bIsCompleted = true;\
+				break;\
+			case ECalculationType::Lerp :\
+				Value = Config.TYPE##BaseValue + (Config.TYPE##TargetValue - Config.TYPE##BaseValue) * Config.EffectTimer / Config.EffectLength;\
+				break;\
+			case ECalculationType::Curve :\
+				Value = Config.TYPE##BaseValue + (Config.TYPE##TargetValue - Config.TYPE##BaseValue) * Config.FloatCurve->GetFloatValue(Config.EffectTimer / Config.EffectLength);\
+				break;\
+		}\
+		PostProcessSettings.ATTRIBUTENAME = Value;\
+	}; \
+	End =  [&](FPostProcessSettings& PostProcessSettings, FPostProcessControllerConfig& Config){\
+		if(Config.bUseBaseValueAsReturnValue){\
+			PostProcessSettings.ATTRIBUTENAME = Config.TYPE##BaseValue;\
+		}\
+		else{\
+			PostProcessSettings.ATTRIBUTENAME = Config.TYPE##ReturnValue;\
+		}\
+	};\
+	break;\
+
+UENUM()
+enum class EPPFXGlobalEvent : uint8
+{
+	PPFX_BeginPlay
+};
+
+UENUM(BlueprintType)
+enum class EPostProcessAttributes : uint8
+{
+	WhiteTemp UMETA(DisplayName = "Temperature (float)"),
+	SceneColorTint UMETA(DisplayName = "Color Tint (FLinearColor)"),
+	ColorSaturation UMETA(DisplayName = "Color Saturation (FVector4)")
+};
+
+UENUM(BlueprintType)
+enum class EPostProcessAttributeType : uint8
+{
+	Float,
+	FVector4,
+	FLinearColor
+};
+
+UENUM(BlueprintType)
+enum class EOperationType : uint8
+{
+	Override,
+	Additive,
+	Multiply
+};
+
+UENUM(BlueprintType)
+enum class ECalculationType : uint8
+{
+	Instant,
+	Lerp,
+	Curve
+};
+
+USTRUCT(BlueprintType)
+struct FPostProcessControllerConfig
+{
+	GENERATED_BODY()
+
+	int Priority = 0;
+
+	UPROPERTY(EditAnywhere)
+	EPostProcessAttributes Attribute;
+
+	UPROPERTY(EditAnywhere)
+	EPostProcessAttributeType AttributeType;
+
+	UPROPERTY(EditAnywhere)
+	EOperationType OperationType;
+	
+	UPROPERTY(EditAnywhere)
+	ECalculationType CalculationType;
+
+	UPROPERTY(EditAnywhere, meta = (EditConditionHides, EditCondition = "CalculationType == ECalculationType::Curve"))
+	TObjectPtr<UCurveFloat> FloatCurve;
+
+	UPROPERTY(EditAnywhere, meta = (EditConditionHides, EditCondition = "AttributeType == EPostProcessAttributeType::FVector4"))
+	FVector4 FVector4Value;
+
+	UPROPERTY()
+	FVector4 FVector4BaseValue;
+	
+	UPROPERTY(EditAnywhere, meta = (EditConditionHides, EditCondition = "AttributeType == EPostProcessAttributeType::FVector4 && !bUseBaseValueAsReturnValue"))
+	FVector4 FVector4ReturnValue;
+
+	UPROPERTY()
+	FVector4 FVector4TargetValue;
+	
+	UPROPERTY(EditAnywhere, meta = (EditConditionHides, EditCondition = "AttributeType == EPostProcessAttributeType::FLinearColor"))
+	FLinearColor FLinearColorValue;
+
+	UPROPERTY()
+	FLinearColor FLinearColorBaseValue;
+	
+	UPROPERTY(EditAnywhere, meta = (EditConditionHides, EditCondition = "AttributeType == EPostProcessAttributeType::FLinearColor && !bUseBaseValueAsReturnValue"))
+	FLinearColor FLinearColorReturnValue;
+
+	UPROPERTY()
+	FLinearColor FLinearColorTargetValue;
+
+	UPROPERTY(EditAnywhere, meta = (EditConditionHides, EditCondition = "AttributeType == EPostProcessAttributeType::FLinearColor"))
+	float floatValue = 0;
+
+	UPROPERTY()
+	float floatBaseValue = 0;
+
+	UPROPERTY(EditAnywhere, meta = (EditConditionHides, EditCondition = "AttributeType == EPostProcessAttributeType::Float && !bUseBaseValueAsReturnValue"))
+	float floatReturnValue = 0;
+	
+	UPROPERTY()
+	float floatTargetValue = 0;
+
+	bool bIsBaseValueInitialized = false;
+	bool bIsTargetValueInitialized = false;
+
+	UPROPERTY(EditAnywhere)
+	bool bUseBaseValueAsReturnValue = false; 
+	
+	UPROPERTY(EditAnywhere)
+	float EffectDelay = 0;
+	
+	UPROPERTY()
+	float DelayTimer = 0;
+	
+	UPROPERTY(EditAnywhere)
+	float EffectLength = 0;
+	
+	UPROPERTY()
+	float EffectTimer = 0;
+
+	bool bIsCompleted = false;
+	
+	TFunction<void(FPostProcessSettings& PostProcessSettings, FPostProcessControllerConfig& Config, float DeltaTime)> Process;
+	
+	TFunction<void(FPostProcessSettings& PostProcessSettings, FPostProcessControllerConfig& Config)> End;
+	
+	void PickProcessFunction()
+	{
+		switch (Attribute) {
+			POST_PROCESS_ATTRIBUTE_CASE(float, WhiteTemp)
+			POST_PROCESS_ATTRIBUTE_CASE(FLinearColor, SceneColorTint)
+			POST_PROCESS_ATTRIBUTE_CASE(FVector4, ColorSaturation)
+		}
+	}
+};
+
+USTRUCT(BlueprintType)
+struct FPostProcessControllerConfigContainer
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	EPPFXGlobalEvent GlobalEventType;
+	
+	UPROPERTY(EditAnywhere)
+	int Priority = 0;
+
+	UPROPERTY(EditAnywhere)
+	bool bTerminateOtherRunningConfigsOnActivate = false;
+
+	UPROPERTY(EditAnywhere)
+	TArray<FPostProcessControllerConfig> Configs;
+};
+
+USTRUCT()
+struct FPostProcessControllerConfigContainers
+{
+	GENERATED_BODY()
+	
+	TArray<FPostProcessControllerConfigContainer> ConfigContainers;
+};
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class PVD_API UPVDPostProcessController : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	UPVDPostProcessController();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
+	                           FActorComponentTickFunction* ThisTickFunction) override;
+	virtual void EndPlay(EEndPlayReason::Type EndPlayReason) override;
+	
+	UFUNCTION()
+	void CategorizeConfigsWithEvents();
+	
+	UFUNCTION()
+	void Run(FPostProcessControllerConfigContainers Containers);
+
+	void Run(FPostProcessControllerConfig& Config);
+	
+	UFUNCTION()
+	void ProcessConfigs(float DeltaTime);
+
+private:
+	FPostProcessSettings* PostProcessSettings;
+	
+	UPROPERTY(EditAnywhere)
+	TArray<FPostProcessControllerConfigContainer> ConfigContainers;
+	
+	FPostProcessControllerConfig* ActiveRunningPostProcessControllerConfig = nullptr;
+	
+	UPROPERTY()
+	TMap<EPPFXGlobalEvent, FPostProcessControllerConfigContainers> EventConfigContainerMap;
+	
+	UPROPERTY()
+	int CurrentPriorityLevel;
+
+	TArray<FPostProcessControllerConfig> RunningPostProcessControllerConfigs;
+};
